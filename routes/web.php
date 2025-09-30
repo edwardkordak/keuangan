99c5c14d8ddb85fd8a91947d0adcc0d7b2344866
edwardkordak <edwardkordak@gmail.com>
2025-09-30 17:15:22 +0800

    done

--- routes/web.php
+++ routes/web.php
@@ -70,7 +70,7 @@ Route::middleware(['auth', 'role:admin'])->group(function () {
     Route::delete('/workflows/{workflow}/delete', [DocumentWorkflowController::class, 'destroy'])->name('workflows.destroy');
 
     // Progress
-    Route::get('/documents/{id}', [TrackingController::class, 'show'])
+    Route::get('/progress/{id}', [DocumentProgressController::class, 'show'])
         ->whereNumber('id')->name('progress.show');
 
     // Update banyak progress dalam 1 dokumen
